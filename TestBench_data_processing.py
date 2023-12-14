4d071a7bc294cad283c70f7532f44d670d66a76e
9nugunu <9nugunu@korea.ac.kr>
2023-12-14 16:15:08 +0900

    add) add_col_axis docu

--- TestBench_data_processing.py
+++ TestBench_data_processing.py
@@ -12,9 +12,14 @@ def PlotSettings():
     plt.rcParams["font.family"] = 'Times New Roman'
 
 def add_col_axis(number_interval, step, max_point):
+    '''
+    측정 데이터에 x, y original 값 추가하는 함수
+    모터기준 -point ~ +point 이동 기준
+    반대방향으로 이동 시 할당값 변경 필요
+    '''
     x = [i for i in np.arange(max_point, -max_point-step, -step)]
     repeated_x = x * number_interval
-    print(len(repeated_x))
+    # print(len(repeated_x))
     y = [i for i in x for _ in range(number_interval)]
     # row_data['x'], row_data['y'] = repeated_x, y
 

6b3622675d0bb8ee61d516c7ba7605412f31743d
9nugunu <9nugunu@korea.ac.kr>
2023-12-14 15:38:45 +0900

    add) Target data plot

--- Cal_BPM.py
+++ Cal_BPM.py
@@ -1,19 +1,29 @@
 import os
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 print(os.getcwd())
 
 Port = '4port/'
+Wanted_data_x = ' X(C)'
+Wanted_data_y = ' Y(C)'
 
 FileName = 'cal_paper__' + '1' + '_4port_01_0.25.csv'
 file_dir = './-5_5_dataset/' + Port + 'FOR_PAPER/' + FileName # 'PAPER_ONLY_0825/' +
 data = pd.read_csv(file_dir, index_col=False)
 
-data[' X(E)'] = np.log(data[' 2Ch'] * data[' 3Ch']) - (np.log(data[' 1Ch'] * data[' 4Ch']))
-data[' Y(E)'] = np.log(data[' 2Ch'] * data[' 1Ch']) - (np.log(data[' 3Ch'] * data[' 4Ch']))
-data.head()
 data.drop([' Time', ' Type', ' 1Ch', ' 2Ch',  ' 3Ch', ' 4Ch', ' X(A)', ' X(B)', ' Y(A)', ' Y(B)'], axis=1, inplace=True)
 # number_interval, data['x'], data['y'] = add_col_axis(data, step, measure_range)
 
 print(data.head())
+
+plt.scatter(data[Wanted_data_x], data[Wanted_data_y])
+# plt.yticks([0.4, 0.3, 0.2, 0.1, 0.0, -0.1])
+plt.title('measured raw data')
+plt.xlabel('X raw data')
+plt.ylabel('Y raw data')
+# plt.savefig(file_dir+'RAW.png',
+#     format='png',
+#     dpi=1000,
+# bbox_inches='tight')
+plt.show()

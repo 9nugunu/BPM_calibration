114d4bd16108e38a325e57cc087742c84275d2d3
9nugunu <9nugunu@korea.ac.kr>
2023-12-20 16:36:53 +0900

    Rev_신호크기 할당

--- Remote_siggen.py
+++ Remote_siggen.py
@@ -1,6 +1,29 @@
+# default ip: 169.254.2.20
 import pyvisa as visa
+import time
 
 rm_siggen = visa.ResourceManager()
-SGC = rm_siggen.open_resource('TCPIP::192.168.0.1::INSTR')
-print(SGC.query('*IDN?'))
+SGC = rm_siggen.open_resource("TCPIP::192.168.0.100::INSTR")
+print(SGC.query("*IDN?"))
 print(SGC.query("SYST:ERR?"))
+print(SGC.query("SOUR:FREQ?"))
+
+SGC.write("SOUR:POW 20 dBm")
+freq_list = [65, 200, 352]
+
+for freq in freq_list:
+    SGC.write(f"SOUR:FREQ {freq} MHz")
+    print(f"Current freq. {SGC.query('SOUR:FREQ?')}")
+    time.sleep(3)
+if freq == 352 and float(SGC.query("SOUR:POW?")) != 14.0:
+    SGC.write("SOUR:POW 14 dBm")
+    print(f"Current power. {SGC.query('SOUR:POW:POW?')}")
+    time.sleep(3)
+else:
+    SGC.write("SOUR:POW 20 dBm")
+    time.sleep(3)
+
+print(float(SGC.query("SOUR:POW:POW?")))
+print(SGC.query("SOUR:FREQ?"))
+SGC.write("SOUR:POW 1 dBm")
+# SGC.write("OUTP ON")
